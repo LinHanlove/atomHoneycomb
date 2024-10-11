260b1b0820997f16e39612db0dee7880fe585a1c
luwenbin <2188817393@qq.com>
2024-10-11 14:25:31 +0800

    feat: some optimize

--- src/content.tsx
+++ src/content.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react"
 import Cropper from "cropperjs"
+import html2canvas from "html2canvas"
 import type { PlasmoCSConfig } from "plasmo"
 import ReactDOM from "react-dom/client"
 
@@ -16,7 +17,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "areaScreenshot") {
     window.focus()
     areaScreenshot(message.base64)
-
     return true // 表示消息已被处理，不需要再分发
   }
 })
@@ -92,12 +92,21 @@ const areaScreenshot = (base64) => {
     cropImage = await crop(base64, cropContext)
     console.log("裁剪结束--->", cropImage)
 
+    // 删除之前的按钮组
+    const actionContainer = document.querySelector(
+      ".action-container"
+    ) as HTMLElement
+    if (actionContainer) {
+      actionContainer.remove()
+    }
     // 添加操作按钮组
     cropBox.append(
       createButton({
         cropper,
         cropImage,
-        imageContainer
+        imageContainer,
+        cropBox,
+        imageDom
       })
     )
     setBoundary(cropper)
@@ -155,20 +164,27 @@ const crop = (image, opts) => {
     const h = opts.h
     const format = opts.format || "png"
     const img = new Image()
+    img.id = "crop-img"
     img.onload = () => {
       const canvas = document.createElement("canvas")
+      canvas.id = "crop-canvas"
+      const canvasOnce = document.getElementById("crop-canvas")
       canvas.width = w
       canvas.height = h
       canvas.style.position = "relative"
       const context = canvas.getContext("2d")
       context.drawImage(img, x, y, w, h, 0, 0, w, h)
       const cropped = canvas.toDataURL(`image/${format}`)
+      if (canvasOnce) {
+        canvasOnce.remove()
+      }
       document.body.append(canvas)
-      canvas.remove()
       resolve(cropped)
     }
 
     img.src = image
+    document.body.append(img)
+    console.log("img.src--->", img)
   })
 }
 
@@ -176,7 +192,7 @@ const crop = (image, opts) => {
  * 将图片复制进用户粘贴板
  * @param image base64
  */
-const copy_img_to_clipboard = async (image) => {
+const copyImgToClipboard = async (image) => {
   const storage_data = await chrome.storage.sync.get(["model"])
   const model = storage_data.model || "file"
   // 复制都用户粘贴板中
@@ -205,7 +221,7 @@ const createButton = (option) => {
   const buttonContainer = document.createElement("div")
   buttonContainer.className = "action-container"
   buttonContainer.style.position = "absolute"
-  buttonContainer.style.bottom = "-40px"
+  buttonContainer.style.bottom = "-50px"
   buttonContainer.style.right = "0"
   buttonContainer.style.width = "auto"
   buttonContainer.style.height = "32px"
@@ -232,6 +248,12 @@ const createButton = (option) => {
         width: "auto",
         height: "100%"
       }}>
+      {/* <Icon
+        className="cancel"
+        style={{ ...buttonStyle, color: "#36aef4" }}
+        icon="mynaui:rectangle"
+        onClick={() => draw(option)}
+      /> */}
       <Icon
         className="cancel"
         style={{ ...buttonStyle, color: "#f44336" }}
@@ -256,7 +278,7 @@ const confirmScreenshot = (option) => {
   console.log(option, "确认截图")
   const { cropImage } = option
   if (!cropImage) return
-  copy_img_to_clipboard(cropImage)
+  copyImgToClipboard(cropImage)
   cancelScreenshot(option)
 }
 
@@ -283,4 +305,62 @@ const disableBrowserEvent = () => {
 const enableBrowserEvent = () => {
   // 启用滚动
   document.body.style.overflow = ""
+}
+
+/**
+ * @function 点击绘制
+ * 删除裁切区域内容，替换为canvas 方便绘制
+ */
+const draw = (option) => {
+  const { cropper, imageDom } = option
+
+  // 获取cropper的画布和上下文
+  const canvas = cropper.cropBox
+  console.log(canvas, "canvas")
+
+  // 获取当前裁剪框的数据
+  const rect = cropper.getCropBoxData()
+  // 获取当前的截图
+  const cropImage = cropper.getCroppedCanvas().toDataURL("image/png")
+
+  // 创建一个新的canvas元素
+  const newCanvas = document.createElement("canvas")
+  newCanvas.id = "newCanvas"
+  newCanvas.width = rect.width
+  newCanvas.height = rect.height
+  // 删除裁切区域内容
+  canvas.remove()
+  // 将新的canvas元素添加到页面中
+  canvas.append(newCanvas)
+
+  // const ctx = canvas.getContext("2d")
+
+  // // 保存当前状态
+  // ctx.save()
+
+  // // 绘制一个矩形框
+  // ctx.strokeStyle = "red" // 边框颜色
+  // ctx.lineWidth = 2 // 边框线宽
+  // const rect = cropper.getCropBoxData() // 获取裁剪框数据
+  // ctx.strokeRect(0, 0, rect.width, rect.height) // 绘制矩形框
+
+  // // 恢复之前的状态
+  // ctx.restore()
+
+  // // 将绘制的canvas转换为base64，并设置为图片元素的源
+  // const dataUrl = canvas.toDataURL("image/png")
+
+  // // 创建一个新的Image对象，用于替换裁剪区域的内容
+  // const newImage = new Image()
+  // newImage.onload = () => {
+  //   // 替换裁剪区域的内容
+  //   const imageData = ctx.getImageData(0, 0, rect.width, rect.height)
+  //   const newCanvas = document.createElement("canvas")
+  //   const newCtx = newCanvas.getContext("2d")
+  //   newCanvas.width = rect.width
+  //   newCanvas.height = rect.height
+  //   newCtx.putImageData(imageData, 0, 0)
+  //   cropper.replace(newCanvas.toDataURL("image/png"))
+  // }
+  // newImage.src = dataUrl
 }
